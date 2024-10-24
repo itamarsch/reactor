4967e0541728d2045b2e1f4fb7a7b7ee387f1762
Itamar Schwartz <schwartzitamar7@gmail.com>
2024-10-24 23:18:28 +0300

    Perf code cleanup

--- perf.sh
+++ perf.sh
@@ -3,3 +3,5 @@ cargo build --release
 perf record --call-graph dwarf target/release/wasmy $1
 
 perf script | ~/.cargo/bin/inferno-collapse-perf | ~/.cargo/bin/inferno-flamegraph >perf.svg
+
+firefox perf.svg
